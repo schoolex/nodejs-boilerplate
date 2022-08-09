26690460d509a826e29deb7ed1aa600902a5abbe
Biao Yi <schoolex@hotmail.com>
2022-08-09 18:59:03 +0800

    refactor: remove unused config variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,51 +9,12 @@ if (!envFound) {
 }
 
 export default {
-  /**
-   *  Application port.
-   */
+  // Application port.
   port: parseInt(process.env.PORT) || 3000,
 
-  /**
-   * JWT Secret
-   */
-  jwtSecret: process.env.JWT_SECRET,
 
-  /**
-   * MongoDB connection options.
-   */
+  // DB connection options.
   database: {
-    type: process.env.TYPEORM_CONNECTION,
-    /**
-     * Connection url where perform connection to.
-     */
-    url: process.env.TYPEORM_HOST,
-    /**
-     * Database host.
-     */
-    host: process.env.TYPEORM_HOST,
-    /**
-     * Database host port.
-     */
-    // tslint:disable-next-line: radix
-    port: Number.parseInt(process.env.TYPEORM_PORT),
-    /**
-     * Database username.
-     */
-    username: process.env.TYPEORM_USERNAME,
-    /**
-     * Database password.
-     */
-    password: process.env.TYPEORM_PASSWORD,
-    /**
-     * Database name to connect to.
-     */
-    database: process.env.TYPEORM_DATABASE,
-  },
-
-  agenda: {
-    dbCollection: process.env.AGENDA_DB_COLLECTION,
-    pooltime: process.env.AGENDA_POOL_TIME,
-    concurrency: process.env.AGENDA_CONCURRENCY,
+    url: process.env.DB_URL,
   },
 };
