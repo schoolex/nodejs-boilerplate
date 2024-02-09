1c5932a552f3b81dbc6bb1c9def5f41537e9d7b1
schoolex <schoolex@hotmail.com>
2024-02-09 19:20:23 +0800

    patch vulnerabilities

--- src/repository/userRepo.ts
+++ src/repository/userRepo.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import { User, UserModel, UserUpdateOptions } from '../models/user';
 
 const createUser = async (user: User) => {
@@ -6,21 +7,27 @@ const createUser = async (user: User) => {
 };
 
 const updateUser = async (userId: string, updatedUserFields: UserUpdateOptions) => {
-  const updatedUser = await UserModel.findByIdAndUpdate(userId, updatedUserFields, {
-    new: true,
-  })
+  const updatedUser = await UserModel.findOneAndUpdate(
+    { _id: { $eq: new mongoose.Types.ObjectId(userId) } }, 
+    updatedUserFields, 
+    { new: true }
+  )
     .select('-__v')
     .lean();
   return updatedUser;
 };
 
 const getUser = async (userId: string) => {
-  const user = await UserModel.findById(userId).select('-__v').lean();
+  const user = await UserModel.findOne({ _id: { $eq: new mongoose.Types.ObjectId(userId) } })
+    .select('-__v')
+    .lean();
   return user;
 };
 
 const deleteUser = async (userId: string) => {
-  const deletedUser = await UserModel.findByIdAndDelete(userId).select('-__v').lean();
+  const deletedUser = await UserModel.findOneAndDelete({ _id: { $eq: new mongoose.Types.ObjectId(userId) } })
+    .select('-__v')
+    .lean();
   return deletedUser;
 };
 
